dea81f0b45f9acab1a535b72659029d724bdfaef
George Nguyen <g.nguyen92@yahoo.com>
2017-07-20 11:03:07 -0700

    BUG: js file closing tags not working

---
+++ practice/statelessReactComponent.js
@@ -0,0 +1,43 @@
+
+// Using Javascript to render components *****************************************
+
+
+
+// React.createClass creates a reusable component
+// takes in a "specification object" as its arg
+// the spec object contains all the characteristics of the component
+// every createClass must contain a render to tell the component what to display
+var helloWorld = React.createClass({
+	render: function() {
+		return React.createElement('h3', {className: 'highlight'}, 'Hello world, from a component!');
+	}
+});
+
+      var hellowWorldJSX = React.createClass({
+        render: function() {
+          return (
+              <p>Hello, Universe</p>
+          )
+        }
+      });
+
+
+// Creates the element above and renders it to screen
+var reactComponentElement = React.createElement(helloWorld);
+ReactDOM.render(reactComponentElement, document.getElementById('reactClass1'));
+
+ReactDOM.render(<helloWorld /> , document.getElementById('reactClass2'));
+ReactDOM.render(<helloWorldJSX /> , document.getElementById('reactClass3'));
+
+
+// Using JSX to render components ***********************************************
+//var helloWorldJSX = React.createClass({
+//	render: function() {
+//		return <h3 className='highlight-jsx'>
+//            Hello world, from a component using JSX!
+//        </h3>   
+//    }
+//});
+
+
+
