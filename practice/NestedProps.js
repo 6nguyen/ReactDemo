295d93dfea96fd1a19665a957d5d952aa470ffc0
George Nguyen <g.nguyen92@yahoo.com>
2017-07-20 14:52:06 -0700

    NestedProp.js: Add var Container2 to hold simple property and javascript expressions

--- practice/NestedProps.js
+++ practice/NestedProps.js
@@ -3,33 +3,47 @@
     Container is the parent container which DOES NOT use the getDefaultProps method.
     Instead, it creates a few properties INTO Contents, the child
 */
-
-var Container = React.createClass({
-   
-    render: function() {
+// Container1 creates properties into Contents
+var Container1 = React.createClass({
+    render: function () {
         return (
             <div className="container">
                 <Contents text = "These properties are in the parent container" />
                 <Contents text = "But they're being accessed by the child container"/>
                 <Contents text = "Hard-coded text child" />
-                <Contents text = {"Javascript expression text child"} />
             </div>
         );
     }
-    
 });
 
 
+// Container 2 simply has a property, "thing", and creates javascript expressions into Context
+var Container2 = React.createClass({
+    thing: "This text is just a property of Container2",
+    render: function() {
+       return (
+            <div className="container2">
+                <Contents text = {"This container contains javascript expressions"} />
+                <Contents text = {this.thing} />
+                <Contents text = {1+2+3+4+5} />
+            </div>
+       );
+   } 
+});
 
+
+// Contents inherits all the properties from Container1 and Container2 and displays
+// its parents' properties onto the screen.
 var Contents = React.createClass({
    render: function() {
        return (
-           <div className="outer">
-                {this.props.text}           
+           <div className="contents">
+                {this.props.text}     
            </div>
        );
    } 
 });
 
 
-ReactDOM.render(<Container />, document.getElementById('container'));
+ReactDOM.render(<Container1 />, document.getElementById('component1'));
+ReactDOM.render(<Container2 />, document.getElementById('component2'));
