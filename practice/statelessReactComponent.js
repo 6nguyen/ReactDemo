9e116c1e8de453c70b1c6e521571e3faa474fdf2
George Nguyen <g.nguyen92@yahoo.com>
2017-07-20 11:58:17 -0700

    statelessReactComponent: File runs.  Bug persists

--- practice/statelessReactComponent.js
+++ practice/statelessReactComponent.js
@@ -1,8 +1,6 @@
 
 // Using Javascript to render components *****************************************
 
-
-
 // React.createClass creates a reusable component
 // takes in a "specification object" as its arg
 // the spec object contains all the characteristics of the component
@@ -13,31 +11,42 @@ var helloWorld = React.createClass({
 	}
 });
 
-      var hellowWorldJSX = React.createClass({
-        render: function() {
-          return (
-              <p>Hello, Universe</p>
-          )
-        }
-      });
-
-
-// Creates the element above and renders it to screen
-var reactComponentElement = React.createElement(helloWorld);
-ReactDOM.render(reactComponentElement, document.getElementById('reactClass1'));
-
-ReactDOM.render(<helloWorld /> , document.getElementById('reactClass2'));
-ReactDOM.render(<helloWorldJSX /> , document.getElementById('reactClass3'));
 
 
 // Using JSX to render components ***********************************************
-//var helloWorldJSX = React.createClass({
-//	render: function() {
-//		return <h3 className='highlight-jsx'>
-//            Hello world, from a component using JSX!
-//        </h3>   
-//    }
-//});
+var helloUniverse = React.createClass({
+    render: function() {
+      return (
+          <p className='highlight-jsx'>Hello, Universe</p>
+          )
+    }
+  });
+
+var helloWorldJSX = React.createClass({
+	render: function() {
+		return ( 
+            <h3> Hello world, from a component using JSX! </h3>   
+        )
+    }
+});
+
+
+// Creates the element above and renders it to screen ***************
+var reactComponentElement = React.createElement(helloWorld);
+var reactComponentElement2 = React.createElement(helloUniverse);
+var reactComponentElement3 = React.createElement(helloWorldJSX);
+ReactDOM.render(reactComponentElement, document.getElementById('reactClass1'));
+ReactDOM.render(reactComponentElement2, document.getElementById('reactClass2'));
+ReactDOM.render(reactComponentElement3, document.getElementById('reactClass3'));
+
+
+// Render elements by directly calling var name **********
+// ********** BUG NOT WORKING **********************************
+ReactDOM.render(<helloWorld /> , document.getElementById('reactClass4'));
+//ReactDOM.render(<helloUniverse /> , document.getElementById('reactClass4'));
+
+
+
 
 
 
