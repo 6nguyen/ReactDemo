96cfa6fd260f4f8df8a4da29855697ab1f687f83
George Nguyen <g.nguyen92@yahoo.com>
2017-07-21 20:32:10 -0700

    StatefulBox.js: Add button+func to chang bg color, both text & bg color. Change text in box to state variable

--- practice/StatefulBox.js
+++ practice/StatefulBox.js
@@ -10,19 +10,51 @@ var Box = React.createClass({
     // getInitialState determines what state a component starts in
     getInitialState: function() {
         return {
+            text: "Hello REACT World",
             textColor: "white",
             backgroundColor: "darkslategrey"
         }
     },
     
     // Randomly changes text color
+        // Need to update the state with the new text color and re-render the box
+        // setState automatically re-renders this component AND ALL ITS child components
+        // Takes an object as its arg, only specify props that have changed
+        // Don't need to include backgroundColor since it hasn't changed
     handleTextColorClick: function() {
         var colors = ['gold', 'lawngreen', 'orangered', 'fuchsia', 'black'];
         var colorIndex = Math.floor(Math.random()*colors.length);
+        
         this.setState({
             textColor: colors[colorIndex]             
         });
     },
+    
+    
+    // Randomly changes background color
+    handleBackgroundColorClick: function() {
+        var colors = ['white', 'blue', 'purple', 'aquamarine', 'indianred'];
+        var colorIndex = Math.floor(Math.random()*colors.length);
+        
+        this.setState({
+           backgroundColor: colors[colorIndex] 
+        });
+    },
+    
+    
+    // Randomly changes both text and background color
+    handleBothColorClick: function() {
+        var colors = ['white', 'blue', 'purple', 'aquamarine', 'indianred',
+                     'gold', 'lawngreen', 'orangered', 'fuchsia', 'black'];
+        var textIndex = Math.floor(Math.random()*colors.length);
+        var bgIndex = Math.floor(Math.random()*colors.length);
+        
+        this.setState({
+           backgroundColor: colors[textIndex],
+            textColor: colors[bgIndex]
+        });
+    },
+        
     
     
     // Object divStyle is all the properties we want to apply to the box
@@ -30,18 +62,26 @@ var Box = React.createClass({
     render: function() {
         const divStyle = {
             color: this.state.textColor,
-            backgroundColor: this.state.backgroundColor
+            backgroundColor: this.state.backgroundColor 
         };
         
         // create a box with the styles from divStyle
         return (
             <div>
                 <div className="box" style={divStyle}>
-                    Hello REACT World!
+                    {this.state.text}
                 </div>
-            
+            <br/>
                 <button className="button" onClick={this.handleTextColorClick}>
                     Change Text Color
+                </button>
+            <br/><br/>
+                <button className="button" onClick={this.handleBackgroundColorClick}>
+                    Change Background color
+                </button>
+            <br/><br/>
+                <button className="button" onClick={this.handleBothColorClick}>
+                    Change BOTH?!
                 </button>
             </div>
 
