74f661796ad487a9e8fc6e12f5ff48ec6d9051de
George Nguyen <g.nguyen92@yahoo.com>
2017-07-22 21:23:13 -0700

    CommentApp: create html and js files

---
+++ practice/CommentApp.js
@@ -0,0 +1,14 @@
+/*
+	A commenting web app that allows users to type in a comment and see it displayed
+	below.  Comments can be deleted once they are posted.
+	UI files are kept in
+*/
+
+// Step 1:  Create the Comment component
+var Comment = React.createClass ({
+
+
+	// Step 2:  Render the 
+
+
+});
